93ddd43e977c744108c23585cfaa8f2e77aba98e
Ivan Lopez <lopezivan256@gmail.com>
2024-06-05 22:21:47 -0700

    6/5 upload

---
+++ code/helper_functions.py
@@ -0,0 +1,301 @@
+import sys
+import os
+import re
+import json
+import argparse
+import glob
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import pandas as pd
+import time
+import pyperclip
+from datetime import datetime
+import pytz
+
+def send_data_and_get_output(
+        driver,
+        prompt, 
+        input_text_lag_time,
+        generation_sleep_timer,
+        max_data_loading_retries,
+        retry_data_loading_wait_time,
+        max_chat_dialogs,
+        global_iteration,
+        total_iterations,
+        llm_output,
+        prompt_column_name,
+        output_column_name,
+        disclaimer_statement,
+        terms_to_avoid,
+        min_output_word_count,
+):
+    if global_iteration == 1 and disclaimer_statement is not None:
+        print(f"Sending disclaimer statement")
+        search_bar = driver.find_element(By.CSS_SELECTOR, 'textarea.flex.w-full')
+        search_bar.clear()
+        search_bar.send_keys(disclaimer_statement)
+        time.sleep(5)
+        click_send_data_button(driver)
+        time.sleep(generation_sleep_timer)
+
+    search_bar = driver.find_element(By.CSS_SELECTOR, 'textarea.flex.w-full') # Find the search bar
+    search_bar.clear()  # Clearing the search bar before sending new data
+    processed_data = prompt.replace('\n', ' ') # Remove or replace newline characters, then add a newline at the end
+    search_bar.send_keys(processed_data) # Send the data to the search bar
+    time.sleep(input_text_lag_time) # Wait for input to be sent to search bar
+    click_send_data_button(driver) # Click send data button
+    time.sleep(generation_sleep_timer) # Wait for the output to be displayed
+
+    retries = 0
+    while is_loading_present(driver) and retries < max_data_loading_retries:
+        retries += 1
+        print(f"Count {retries}: Waiting for data to load...")
+        time.sleep(retry_data_loading_wait_time)
+
+    # Find all elements that match the output structure
+    elements_start = driver.find_elements(By.XPATH, "//*[contains(@class, 'container mx-auto max-w-4xl py-6 flex flex-col items-start')]")
+    elements_end = driver.find_elements(By.XPATH, "//*[contains(@class, 'container mx-auto max-w-4xl py-6 flex flex-col items-end')]")
+
+    if retries < max_data_loading_retries:        
+        # Assuming the newest output is always last, get the last element's text and dialog sent
+        latest_output = elements_start[-1].text.replace('Secure GPT (Beta)\n', '').replace('\n', ' ').lower()
+        latest_send = elements_end[-1].text.replace('Lopez Rodriguez, Ivan (SU)\n', '')
+        word_count = len(latest_output.split())
+        
+        if global_iteration != 1:
+            # Validate latest_output for content moderation failure
+            latest_output, latest_send = validate_latest_dialog_sent(
+                latest_output=latest_output, 
+                latest_send=latest_send, 
+                llm_output=llm_output, 
+                prompt_column_name=prompt_column_name, 
+                output_column_name=output_column_name, 
+                processed_data=processed_data
+                )
+    elif retries >= max_data_loading_retries:
+        print(f"Data loading retries exceeded. Setting output as 'DATA_LOAD_FAILURE'")
+        latest_output = "DATA_LOAD_FAILURE"
+        latest_send = elements_end[-1].text.replace('Lopez Rodriguez, Ivan (SU)\n', '')
+        word_count = len(latest_output.split()) # recalculating word count for 'DATA_LOAD_FAILURE' to initiate check_for_terms_and_resend_data_if_needed() loop
+    
+    print(f"Checking LLM Output for terms to avoid...")
+    latest_output, latest_send, word_count = check_for_terms_and_resend_data_if_needed(
+        latest_output=latest_output,
+        latest_send=latest_send,
+        disclaimer_statement=disclaimer_statement,
+        terms_to_avoid=terms_to_avoid,
+        min_output_word_count=min_output_word_count,
+        word_count=word_count,
+        driver=driver,
+        prompt=prompt, 
+        input_text_lag_time=input_text_lag_time,
+        generation_sleep_timer=generation_sleep_timer,
+        max_data_loading_retries=max_data_loading_retries,
+        retry_data_loading_wait_time=retry_data_loading_wait_time,
+        global_iteration=global_iteration,
+        llm_output=llm_output,
+        prompt_column_name=prompt_column_name,
+        output_column_name=output_column_name,
+        )
+    print(f"LLM Output:\n{latest_output}")
+
+    # # validate latest_output:
+    # latest_output = validate_output_text(latest_output)
+
+    # Create new chat window (if statement)
+    if disclaimer_statement is not None:
+        total_outputs = len(elements_start)-1
+    else:
+        total_outputs = len(elements_start)
+    if total_outputs % max_chat_dialogs == 0 and global_iteration != total_iterations:
+            print("\n\nCreating new chat window...")
+            click_new_chat_button(driver=driver)
+            time.sleep(3)
+            click_balanced_button(driver=driver)
+            # time.sleep(3)
+            # click_file_button(driver=driver)
+            print("New chat window created\n\n")
+            time.sleep(5)
+
+            if disclaimer_statement is not None:
+                print(f"Sending disclaimer statement")
+                search_bar = driver.find_element(By.CSS_SELECTOR, 'textarea.flex.w-full')
+                search_bar.clear()
+                search_bar.send_keys(disclaimer_statement)
+                time.sleep(5)
+                click_send_data_button(driver)
+                time.sleep(generation_sleep_timer)
+
+    return latest_output, latest_send
+
+def check_for_terms_and_resend_data_if_needed(
+        latest_output,
+        latest_send,
+        disclaimer_statement,
+        terms_to_avoid,
+        min_output_word_count,
+        word_count,
+        driver,
+        prompt,
+        input_text_lag_time,
+        generation_sleep_timer,
+        max_data_loading_retries,
+        retry_data_loading_wait_time,
+        global_iteration,
+        llm_output,
+        prompt_column_name,
+        output_column_name,
+):
+    while any(term in latest_output for term in terms_to_avoid) or word_count <= min_output_word_count:
+        print(f"LLM Inference Error:\n{latest_output}")
+
+        # Reset process
+        print("\n\nCreating new chat window...")
+        click_new_chat_button(driver=driver)
+        time.sleep(3)
+        click_balanced_button(driver=driver)
+        print("New chat window created\n\n")
+        time.sleep(5)
+
+        print(f"\nSending disclaimer statement")
+        search_bar = driver.find_element(By.CSS_SELECTOR, 'textarea.flex.w-full')
+        search_bar.clear()
+        search_bar.send_keys(disclaimer_statement)
+        time.sleep(5)
+        click_send_data_button(driver)
+        time.sleep(generation_sleep_timer)
+
+        # Resend data process
+        search_bar = driver.find_element(By.CSS_SELECTOR, 'textarea.flex.w-full')
+        search_bar.clear()
+        processed_data = prompt.replace('\n', ' ')
+        search_bar.send_keys(processed_data)
+        time.sleep(input_text_lag_time)
+        click_send_data_button(driver)
+        time.sleep(generation_sleep_timer)
+
+        retries = 0
+        while is_loading_present(driver) and retries < max_data_loading_retries:
+            retries += 1
+            print(f"Count {retries}: Waiting for data to load...")
+            time.sleep(retry_data_loading_wait_time)
+
+        # Find all elements that match the output structure
+        elements_start = driver.find_elements(By.XPATH, "//*[contains(@class, 'container mx-auto max-w-4xl py-6 flex flex-col items-start')]")
+        elements_end = driver.find_elements(By.XPATH, "//*[contains(@class, 'container mx-auto max-w-4xl py-6 flex flex-col items-end')]")
+
+        if retries < max_data_loading_retries:        
+            # Assuming the newest output is always last, get the last element's text and dialog sent
+            latest_output = elements_start[-1].text.replace('Secure GPT (Beta)\n', '').replace('\n', ' ').lower()
+            latest_send = elements_end[-1].text.replace('Lopez Rodriguez, Ivan (SU)\n', '')
+            word_count = len(latest_output.split())
+            
+            if global_iteration != 1:
+                # Validate latest_output for content moderation failure
+                latest_output, latest_send = validate_latest_dialog_sent(
+                    latest_output=latest_output, 
+                    latest_send=latest_send, 
+                    llm_output=llm_output, 
+                    prompt_column_name=prompt_column_name, 
+                    output_column_name=output_column_name, 
+                    processed_data=processed_data
+                    )
+        elif retries >= max_data_loading_retries:
+            print(f"Data loading retries exceeded. Setting output as 'DATA_LOAD_FAILURE'")
+            latest_output = "DATA_LOAD_FAILURE"
+            latest_send = elements_end[-1].text.replace('Lopez Rodriguez, Ivan (SU)\n', '')
+            word_count = len(latest_output.split()) # recalculating word count for 'DATA_LOAD_FAILURE' to initiate check_for_terms_and_resend_data_if_needed() loop
+    return latest_output, latest_send, word_count
+
+def validate_latest_dialog_sent(
+        latest_output,
+        latest_send,
+        llm_output,
+        prompt_column_name,
+        output_column_name,
+        processed_data,
+):
+    # Checking last recorded dialog and latest dialog sent for content moderation failure
+    last_send_with_recorded_output = llm_output[llm_output[output_column_name] != 'NA'][prompt_column_name].iloc[-1]
+    
+    # Check if the latest dialog sent matches the last recorded dialog
+    if last_send_with_recorded_output == latest_send:
+        # Now check if this duplication is unexpected (i.e., not just repeating processed_data)
+        if clean_processed_data(latest_send) != clean_processed_data(processed_data):
+            print(f"***Content moderation failure detected***")
+            print(f"Last recorded dialog and latest dialog sent are the same - setting latest_output to 'NA'")
+            latest_output = "NA"  # Indicating a need to discard this latest output due to moderation failure
+            latest_send = processed_data  # Aligning latest_send with processed_data to attempt a 'correction'
+    # Always return latest_output and latest_send, potentially adjusted
+    return latest_output, latest_send
+
+def click_balanced_button(driver):
+    # Assuming 'Balanced' is unique text on the button
+    balanced_button_xpath = "//button[contains(., 'Balanced')]"
+    balanced_button = driver.find_element(By.XPATH, balanced_button_xpath)
+    balanced_button.click()
+
+def click_new_chat_button(driver):
+    # Specific CSS selector targeting the parent div and then the button
+    # This assumes the combination of div classes is unique to this button
+    specific_button_css_selector = 'div.flex.pb-2.items-center.justify-end > button.inline-flex.items-center.justify-center'
+    specific_button = driver.find_element(By.CSS_SELECTOR, specific_button_css_selector)
+    specific_button.click()
+
+def attempt_send_action(driver):
+    if not click_send_data_button(driver):
+        # If clicking the send button fails, try to close the notification box
+        if close_notification_box(driver):
+            # If successfully closed the notification, try clicking the send button again
+            click_send_data_button(driver)
+        else:
+            # If could not close the notification, wait for user input to proceed
+            print("Could not handle the notification box; further action might be needed.")
+            user_input = input("Please fix the issue and type 1 to continue: ")
+            if user_input == "1":
+                # Try clicking the send button again after the user indicates the problem is fixed
+                click_send_data_button(driver)
+            else:
+                print("Incorrect input. Exiting the function.")
+
+def close_notification_box(driver):
+    try:
+        # Wait for the notification box to be visible, but only wait for 1 second
+        # Adjust the XPath to use a more direct approach if the button has a unique identifier like `toast-close`
+        close_button = WebDriverWait(driver, 1).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[toast-close]"))
+        )
+        close_button.click()
+        print("Notification box closed.")
+        return True  # Successfully closed the notification
+    except TimeoutException:
+        print("No notification box found or could not close in time.")
+        return False  # Failed to close the notification
+
+def click_send_data_button(driver):
+    send_button_css_selector = "button[type='submit'] svg.lucide.lucide-send"
+    send_button = driver.find_element(By.CSS_SELECTOR, send_button_css_selector)
+    send_button.click()
+
+def is_loading_present(driver):
+    try:
+        # Check if the loading spinner is present
+        loading_element = driver.find_element(By.XPATH, "//*[contains(@class, 'lucide lucide-loader animate-spin')]")
+        return loading_element is not None
+    except NoSuchElementException:
+        return False
+
+def clean_processed_data(processed_data):
+    # Replace one or more whitespace characters (including spaces, tabs, newlines) with a single space
+    cleaned_data = re.sub(r'\s+', ' ', processed_data)
+    
+    # Remove leading and trailing whitespace
+    cleaned_data = cleaned_data.strip()
+    
+    return cleaned_data
---
+++ code/run_securegpt_bot.py
@@ -0,0 +1,191 @@
+import sys
+import os
+import re
+import json
+import argparse
+import glob
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import pandas as pd
+import time
+import pyperclip
+from datetime import datetime
+import pytz
+
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+sys.path.append('~/stanford_securegpt_bot/code')
+from helper_functions import (
+    send_data_and_get_output,
+    check_for_terms_and_resend_data_if_needed,
+    validate_latest_dialog_sent,
+    click_balanced_button,
+    click_new_chat_button,
+    attempt_send_action,
+    close_notification_box,
+    click_send_data_button,
+)
+
+def gpt4_inference(
+        test,
+        test_sample_size,
+        inference_prompt_data_path,
+        backup_data_path,
+        disclaimer_statement,
+        min_output_word_count,
+        terms_to_avoid,
+        save_filename,
+        save_folder_path,
+        max_chat_dialogs,
+        input_text_lag_time,
+        generation_sleep_timer,
+        max_data_loading_retries,
+        retry_data_loading_wait_time,
+        website_email_input,
+        website_url,
+):
+    llm_inference_timestamp = datetime.now(pytz.utc).astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d_%H:%M:%S')
+
+    print(f"\nExpanding Paths")
+    prompt_filename = os.path.basename(inference_prompt_data_path)
+    inference_prompt_data_path = os.path.expanduser(inference_prompt_data_path)
+    save_filename = save_filename.format(timestamp=llm_inference_timestamp, prompt_filename=prompt_filename)
+    save_folder_path = os.path.expanduser(save_folder_path)
+    print(f"Paths Expanded")
+
+    print(f"\nExpanding Paths")
+    inference_prompt_data_path = os.path.expanduser(inference_prompt_data_path)
+    print(f"Paths Expanded")
+
+    print(f"\nLoading Data")
+    inference_prompt_data = pd.read_csv(inference_prompt_data_path)
+    print(f"Data Loaded")
+    if backup_data_path is not None:
+        print(f"\nLoading Backup Data")
+        saved_backup_data = pd.read_csv(os.path.expanduser(backup_data_path))
+        print(f"Removing Already Processed Data from Inference Prompt Data")
+        inference_prompt_data = inference_prompt_data[~inference_prompt_data['prompt_id'].isin(saved_backup_data['prompt_id'])].reset_index(drop=True)
+    print(f"Data Loaded:\{inference_prompt_data}")
+
+    print(f"\nSetting Up Iteration Data")
+    prompt_list = inference_prompt_data['formatted_prompt'].to_list()
+    prompt_id_list = inference_prompt_data['prompt_id'].to_list()
+    print(f"Data Ready")
+
+    print("\nSetup the Chrome WebDriver")
+    if website_email_input is not None:
+        pyperclip.copy(website_email_input)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print("Going to Website")
+    driver.get(website_url)
+    # Loop until the user inputs '1'
+    user_input = ""
+    while user_input != "1":
+        user_input = input("\nPlease set-up GPT environment. Press 'Enter' after typing '1' to proceed: ")
+        if user_input == "1":
+            print("Proceeding...")
+        else:
+            print("Incorrect input. Please type '1' to proceed: ")
+
+    if test:
+        print(f"\n\n***Loading Testing Environment***\n\n")
+        prompt_list = prompt_list[:test_sample_size]
+        prompt_id_list = prompt_id_list[:test_sample_size]
+
+    print(f"\nGenerating Synthetic Data")
+    original_column_names = inference_prompt_data.columns.tolist()
+    llm_output = pd.DataFrame(columns=original_column_names + ['synthetic_data'])
+    global_iteration = 0
+    total_iterations = len(prompt_list)
+
+    for prompt, prompt_id in zip(prompt_list, prompt_id_list):
+        try:
+            global_iteration += 1
+            print(f"\n\nProcessing iteration {global_iteration}/{total_iterations}")
+            latest_output, latest_send = send_data_and_get_output(
+                driver=driver,
+                prompt=prompt, 
+                input_text_lag_time=input_text_lag_time,
+                generation_sleep_timer=generation_sleep_timer,
+                max_data_loading_retries=max_data_loading_retries,
+                retry_data_loading_wait_time=retry_data_loading_wait_time,
+                max_chat_dialogs=max_chat_dialogs,
+                global_iteration=global_iteration,
+                total_iterations=total_iterations,
+                llm_output=llm_output,
+                prompt_column_name='formatted_prompt',
+                output_column_name='synthetic_data',
+                disclaimer_statement=disclaimer_statement,
+                terms_to_avoid=terms_to_avoid,
+                min_output_word_count=min_output_word_count,
+                )
+
+            current_data = {'prompt_id': prompt_id, 'formatted_prompt': latest_send, 'synthetic_data': latest_output}
+            llm_output = pd.concat([llm_output, pd.DataFrame([current_data])], ignore_index=True)
+        except Exception as e:
+            print("An error occurred. Saving progress...")
+            error_timestamp = datetime.now(pytz.utc).astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d_%H:%M:%S')
+            save_progress_filename = f"llm_output_backup_{error_timestamp}.csv"
+            full_path = os.path.join(save_folder_path, save_progress_filename)
+            llm_output.to_csv(full_path, index=False)
+            print(f"Progress saved to {full_path}")
+            raise e
+    print(f"Synthetic Data Generated")
+
+    print(f"\nQuitting Chrome WebDriver")
+    driver.quit()
+    print(f"Chrome Driver Quit")
+
+    print(f"\nSaving Synthetic Data")
+    if test:
+        save_filename = f"TEST_{save_filename}"
+    full_path = os.path.join(save_folder_path, save_filename)
+    llm_output.to_csv(full_path, index=False)
+    print(f"Synthetic Data Saved to {full_path}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run SecureGPT Bot')
+    parser.add_argument('test', type=int, nargs='?', help='Start testing environment', default=False)
+    parser.add_argument('test_sample_size', type=int, nargs='?', help='', default=5)
+    parser.add_argument('inference_prompt_data_path', type=int, nargs='?', help='')
+    parser.add_argument('backup_data_path', type=int, nargs='?', help='', default=None)
+    parser.add_argument('disclaimer_statement', type=int, nargs='?', help='', default=None)
+    parser.add_argument('min_output_word_count', type=int, nargs='?', help='', default=40)
+    parser.add_argument('terms_to_avoid', type=int, nargs='?', help='python list', default=["As an AI language model", "As a language model", "As a language AI model"])
+    parser.add_argument('save_filename', type=int, nargs='?', help='', default="gpt-4_{timestamp}_{prompt_filename}")
+    parser.add_argument('save_folder_path', type=int, nargs='?', help='', default="~/Downloads")
+    parser.add_argument('max_chat_dialogs', type=int, nargs='?', help='', default=5)
+    parser.add_argument('input_text_lag_time', type=int, nargs='?', help='', default=15)
+    parser.add_argument('generation_sleep_timer', type=int, nargs='?', help='', default=50)
+    parser.add_argument('max_data_loading_retries', type=int, nargs='?', help='', default=10)
+    parser.add_argument('retry_data_loading_wait_time', type=int, nargs='?', help='', default=5)
+    parser.add_argument('website_email_input', type=int, nargs='?', help='', default=None)
+    args = parser.parse_args()
+    
+    website_url = "https://securegpt.stanfordhealthcare.org/chat"
+    gpt4_inference(
+        test=args.test,
+        test_sample_size=args.test_sample_size,
+        inference_prompt_data_path=args.inference_prompt_data_path,
+        backup_data_path=args.backup_data_path,
+        disclaimer_statement=args.disclaimer_statement,
+        min_output_word_count=args.min_output_word_count,
+        terms_to_avoid=args.terms_to_avoid,
+        save_filename=args.save_filename,
+        save_folder_path=args.save_folder_path,
+        max_chat_dialogs=args.max_chat_dialogs,
+        input_text_lag_time=args.input_text_lag_time,
+        generation_sleep_timer=args.generation_sleep_timer,
+        max_data_loading_retries=args.max_data_loading_retries,
+        retry_data_loading_wait_time=args.retry_data_loading_wait_time,
+        website_email_input=args.website_email_input,
+        website_url=website_url,
+        )
